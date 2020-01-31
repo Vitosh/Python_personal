34b3b0956e216e198cdb2bbba78ddcb3837430bb
Vitosh Academy <Vitosh@users.noreply.github.com>
2020-01-31 11:46:57 +0200

    Update 1835_09_Mutant.py

--- PythonTries/1835_09_Mutant.py
+++ PythonTries/1835_09_Mutant.py
@@ -1,5 +1,15 @@
+is_dead = False
+has_escaped = False
+rows_total = -1
+columns_total = -1
+matrix = []
+
 def main():
     
+    global rows_total
+    global columns_total
+    global matrix
+
     rc = input().split()
     rows_total = int(rc[0])
     columns_total = int(rc[1])
@@ -10,98 +20,130 @@ def main():
         matrix.append(list(line_input))
 
     commands_given = list(input())
-    i = 0
+    i = -1
 
-    player_position = detect_position(matrix, rows_total, columns_total, "P")
+    player_position = detect_position("P")
+    current_row = player_position[0]
+    current_column = player_position[1]
+
     result = ""
 
     while True:
+        i += 1
 
         command  =  commands_given[i]
-        i += 1
+
+        player_position = moving_player(current_row, current_column, command)
+        matrix = spread_bunnies()
 
         current_row = player_position[0]
         current_column = player_position[1]
 
-        player_is_alive = is_player_alive(matrix, rows_total, columns_total, "P")
-        player_has_escaped = has_player_escaped(matrix, rows_total, columns_total)
+        if is_dead:
+            result = "dead: {} {}".format(current_row, current_column)
+            break
 
-        if not player_is_alive:
-            result = "dead:"
-            break            
+        if has_escaped:
+            result = "won: {} {}".format(current_row, current_column)
+            break
 
-        player_has_escaped = has_player_escaped(matrix, rows_total, columns_total)
-
-        if player_has_escaped:
-            result = "won:"
-            break      
-
-        new_position = moving_player(matrix, current_row, current_column, command)
-        matrix = fix_matrix(matrix, player_position, new_position)
-        player_position = new_position
-        matrix = spread_bunnies(matrix,rows_total,columns_total)
-
-    
-    print(result)
+       
     print(format_matrix(matrix))
+    print(result)
 
-def fix_matrix(matrix, player_position, new_position):
+def moving_player(current_row, current_column, command):
 
-    old_row = player_position[0]
-    old_column = player_position[1]
-
-    new_row = new_position[0]
-    new_column = new_position[1]
-
-    matrix[old_row][old_column] = "."
-    matrix[new_row][new_column] = "P"
-
-    return matrix
-
-def is_player_alive(matrix, rows_total, columns_total, my_char):
-
-    for row in range(rows_total):
-        for column in range(columns_total):
-            if matrix[row][column] == my_char:
-                return True
-    return False
-
-
-def moving_player(matrix, current_row, current_column, command):
+    global has_escaped
+    global is_dead
+    global matrix
+    
+    matrix[current_row][current_column] = "."
 
     if command == "U":
+        if current_row == 0:
+            has_escaped = True
+            return [current_row, current_column]
+        elif matrix[current_row - 1][current_column] == "B":
+            is_dead = True
+        else:
+            matrix[current_row - 1][current_column]= "P"
         return [current_row - 1, current_column]
+
     elif command == "R":
+        if current_column == columns_total-1:
+            has_escaped = True
+            return [current_row, current_column]
+        elif matrix[current_row][current_column + 1] == "B":
+            is_dead = True
+        else:
+            matrix[current_row][current_column + 1] = "P"
         return [current_row, current_column + 1]
+    
     elif command == "D":
+        if current_row == rows_total - 1:
+            has_escaped = True
+            return [current_row, current_column]
+        elif matrix[current_row + 1][current_column] == "B":
+            is_dead = True
+        else:
+            matrix[current_row + 1][current_column] = "P"
         return [current_row + 1, current_column]
+
     elif command == "L":
+        if current_column == 0:
+            has_escaped = True
+            matrix[current_row][current_column] = "."
+            return [current_row, current_column]
+        elif matrix[current_row][current_column - 1] == "B":
+            is_dead = True
+        else:
+            matrix[current_row][current_column - 1] = "P"
         return [current_row, current_column - 1]
 
-def detect_position(matrix, rows_total, columns_total, my_char):
+def detect_position(my_char):
+
+    global matrix
     
     for row in range(rows_total):
         for column in range(columns_total):
             if matrix[row][column] == my_char:
                 return [row,column]
+    return something_happened
 
-def spread_bunnies(matrix, rows_total, columns_total):
-    
+def spread_bunnies():
+
+    global matrix
+    global rows_total
+    global columns_total
+    global is_dead
+
     for row in range(rows_total):
         for column in range(columns_total):
             if matrix[row][column] == "B":
 
                 if row < (rows_total - 1):
-                    matrix[row + 1][column] = "*"
+                    if matrix[row + 1][column] == "P":
+                        is_dead = True
+                    if matrix[row + 1][column] != "B":
+                        matrix[row + 1][column] = "*"
 
                 if row > 0:
-                    matrix[row - 1][column] = "*"
+                    if matrix[row - 1][column] == "P":
+                        is_dead = True
+                    if matrix[row - 1][column] != "B":
+                        matrix[row - 1][column] = "*"
 
                 if column > 0:
-                    matrix[row][column - 1] = "*"
+                    if matrix[row][column-1] == "P":
+                        is_dead = True
+                    if matrix[row][column - 1] != "B":
+                        matrix[row][column - 1] = "*"
 
                 if column < (columns_total - 1):
-                    matrix[row][column + 1] = "*"
+                    if matrix[row][column+1] == "P":
+                        is_dead = True
+                    if matrix[row][column + 1] != "B":
+                        matrix[row][column + 1] = "*"
                 
     for row in range(rows_total):
             for column in range(columns_total):
@@ -109,15 +151,6 @@ def spread_bunnies(matrix, rows_total, columns_total):
                     matrix[row][column] = "B"
 
     return matrix
-
-def has_player_escaped(matrix, rows_total, columns_total):
-    
-    for row in range(rows_total):
-        for column in range(columns_total):
-            if matrix[row][column] == "P":
-                if row == 0 or row == rows_total or column == 0 or column == columns_total:
-                    return True
-    return False
 
 def format_matrix(my_matrix):
     return '\n'.join(''.join(str(column) for column in row) for row in my_matrix)
