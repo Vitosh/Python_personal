1a40b891552945f366a7640e2f3663e86061c9aa
Vitosh Academy <Vitosh@users.noreply.github.com>
2020-04-20 19:16:31 +0300

    djangogirls

---
+++ YouTube/002_DjangoGirls/djangogirls/djangogirlsvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\vitos\PycharmProjects\PythonAdvanced\djangogirls\djangogirlsvenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
